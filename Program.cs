62579db5928bdc8d2005abc6d5c13a8b9f99f27d
dogito213 <81418891+dogito213@users.noreply.github.com>
2021-09-27 11:07:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _12max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("b=");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("c=");
+            int c = int.Parse(Console.ReadLine());
+            int S = 0;
+
+
+            if(a>0)&&(b>0)&&(c>0)
+            //if (S==a+b+c)
+            //{ Console.WriteLine("lice=" + S); }
+        }
+    }
+}
